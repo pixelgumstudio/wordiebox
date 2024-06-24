1f1a827d25627fd785ad1cbf859ae34cb6273658
wabjay <jefferyjohn230@gmail.com>
2024-06-24 14:14:32 +0100

    Adjust the tap component display

--- src/app/random-word-generator/page.tsx
+++ src/app/random-word-generator/page.tsx
@@ -18,7 +18,9 @@ const RandomWord: FC = () => {
   const [words, setWords] = useState<string[]>([]);
 
   const incrementNumber = () => {
+    if (number < 10) {
     setNumber((prevNumber) => prevNumber + 1);
+    }
   };
 
   const decrementNumber = () => {
@@ -27,11 +29,15 @@ const RandomWord: FC = () => {
     }
   };
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  // const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   const value = parseInt(e.target.value, 10);
+  //   if (!isNaN(value)) {
+  //     setNumber(value);
+  //   }
+  // };
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const value = parseInt(e.target.value, 10);
-    if (!isNaN(value)) {
-      setNumber(value);
-    }
+    setNumber(value);
   };
 
   const generate = async () => {
@@ -79,27 +85,33 @@ const RandomWord: FC = () => {
             <BackButton />
             <h1 className='font-bold text-[#1C1C1C] mx-auto text-center text-24 tablet:text-32 mt-5 tablet:mt-0'>Random Word Generator</h1>
           </div>
-          <p className='flex justify-center items-center text-[#1C1C1C] text-20 tablet:text-24 font-normal text-center'>
-            Generate
-            <div className="flex justify-between items-center px-3 py-2 mx-2 outline-none h-9 w-[72px] bg-transparent border-[#1C1C1C] border shadow-darkbox text-center">
-              <input
-                type="number"
-                name="number"
-                value={number}
-                onChange={handleChange}
-                className="outline-none w-8 bg-transparent text-left "
-              />
-              <div className="right-2 flex flex-col gap-1 items-center mx-auto w-[100px] z-20">
-                <button onClick={incrementNumber} className="rounded-lg transition">
-                  <Image src={UpArrow} width={12} height={12} alt='' />
-                </button>
-                <button onClick={decrementNumber} className="rounded-lg transition">
-                  <Image src={DownArrow} width={12} height={12} alt='' />
-                </button>
-              </div>
-            </div>
-            random words
-          </p>
+          <p className='flex justify-center items-center text-[#1C1C1C] text-20 tablet:text-24 font-normal whitespace-nowrap text-center'>
+  Generate
+  <div className="flex justify-between items-center px-3 py-2 mx-2 outline-none h-9 w-[72px] bg-transparent border-[#1C1C1C] border shadow-darkbox text-center">
+    <select
+      name="number"
+      value={number}
+      onChange={handleChange}
+      className="outline-none w-8 bg-transparent text-left appearance-none"
+    >
+      {[...Array(10)].map((_, index) => (
+        <option key={index + 1} value={index + 1}>
+          {index + 1}
+        </option>
+      ))}
+    </select>
+    <div className="right-2 flex flex-col gap-1 items-center mx-auto w-[100px] z-20">
+      <button onClick={incrementNumber} className="rounded-lg transition">
+        <Image src={UpArrow} width={12} height={12} alt='' />
+      </button>
+      <button onClick={decrementNumber} className="rounded-lg transition">
+        <Image src={DownArrow} width={12} height={12} alt='' />
+      </button>
+    </div>
+  </div>
+  random words
+</p>
+
 
           <p className='p-2 w-full max-w-[360px] cursor-pointer text-center font-medium mx-auto mt-10 h-fit bg-[#FFCC00] border-[#1C1C1C] border shadow-darkbox' onClick={generate}>
             Generate words{loading && "..."}
@@ -107,7 +119,7 @@ const RandomWord: FC = () => {
 
           <div className="flex flex-wrap justify-center w-fit mx-auto gap-5 mt-12">
           {words.length > 0 && words.map((word, index) => (
-              <p key={index} className="inline-block text-black border-[#1C1C1C] bg-[#DFC3FF] border shadow-darkbox p-3 tablet:p-4 text-20 tablet:text-24 capitalize font-semibold hover:bg-[#e2c9ff]">
+            <p key={index} className="text-center w-full max-w-[33.333333%] tablet:max-w-[20%] laptop:max-w-[16.666667%] inline-block text-black border-[#1C1C1C] bg-[#DFC3FF] border shadow-darkbox p-3 tablet:p-4 text-20 tablet:text-24 capitalize font-semibold hover:bg-[#e2c9ff]">
                 {word}
               </p>
             ))}
