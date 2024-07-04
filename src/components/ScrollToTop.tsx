abe48afaf4bb791e89907c911c6230fd351fda72
wabjay <jefferyjohn230@gmail.com>
2024-07-04 12:36:27 +0100

    Added all languages to the website with dynamic seo for it

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,25 +1,25 @@
-"use client"
-// components/ScrollToTop.ts
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+// "use client"
+// // components/ScrollToTop.ts
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
 
-const useScrollToTop = () => {
-  const router = useRouter();
+// const useScrollToTop = () => {
+//   const router = useRouter();
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') { // Ensure this code runs only in the browser
-      const handleRouteChange = () => {
-        window.scrollTo(0, 0);
-      };
+//   useEffect(() => {
+//     if (typeof window !== 'undefined') { // Ensure this code runs only in the browser
+//       const handleRouteChange = () => {
+//         window.scrollTo(0, 0);
+//       };
 
-      router.events.on('routeChangeComplete', handleRouteChange);
+//       router.events.on('routeChangeComplete', handleRouteChange);
 
-      // Cleanup on unmount
-      return () => {
-        router.events.off('routeChangeComplete', handleRouteChange);
-      };
-    }
-  }, [router]);
-};
+//       // Cleanup on unmount
+//       return () => {
+//         router.events.off('routeChangeComplete', handleRouteChange);
+//       };
+//     }
+//   }, [router]);
+// };
 
-export default useScrollToTop;
+// export default useScrollToTop;
