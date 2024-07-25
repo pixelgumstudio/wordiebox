df8c8afde63aac5802d1d880a69d0f8f9e607120
wabjay <jefferyjohn230@gmail.com>
2024-07-25 02:16:22 +0100

    Changed Icons

--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -8,7 +8,7 @@ const BackButton = () => {
   const handleBack = () => {
     router.back(); // Go back to the previous page
   };
-
+ 
     return (
       <button  onClick={handleBack} className={`tablet:absolute tablet:top-0 tablet:left-0 w-fit cursor-pointer flex justify-center items-center text-black border-[#1C1C1C] bg-white border shadow-darkbox py-2 px-2 text-sm font-medium focus:outline-none hover:bg-white`}>
       <Image src={Back} alt="" width={20} height={20} className="rotate-180"/>Back </button>
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 "use client"
 import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
+import { useRouter } from 'next/navigation';
 import React, { ReactNode } from 'react';
 
 interface LayoutProps {
@@ -9,12 +10,17 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children, title }) => {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.back(); // Go back to the previous page
+  };
 
     return (
       <div className='w-full bg-[#FFFFFF]' id='hero'>
       <div className='w-full px-4 tablet:px-6 laptop:px-0 mx-auto py-[50px] tablet:py-[80px] laptop:py-[100px]'>
       <div className="relative text-center mb-8 flex flex-col gap-6 tablet:justify-center tablet:items-center">
-        <p className='text-[#757575] font-medium'>Tools/<span className='text-[#1C1C1C]'>{title}</span></p>
+        <p className='text-[#757575] font-medium'><span className='cursor-pointer' onClick={handleBack}>Tools/</span><span className='text-[#1C1C1C]'>{title}</span></p>
           <h1 className='font-bold text-[#1C1C1C] mx-auto text-center text-24 tablet:text-32 mt-5 tablet:mt-0 capitalize'>{title}</h1>
          </div>
        {children}
