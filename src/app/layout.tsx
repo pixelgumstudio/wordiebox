a092d0a55fdcc3517fec5a5a0151396263d85ba9
Jeffery49 <jefferyjohn230@gmail.com>
2025-03-01 09:01:14 +0100

    Grow.me script added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,12 @@ export default function RootLayout({
         <Footer />
         <GoogleTagManager gtmId="GTM-K8ST54XF" />
         <GoogleAnalytics gaId="G-P8TXFVSRPZ" />
+        <script
+          data-grow-initializer=""
+          dangerouslySetInnerHTML={{
+            __html: `!(function(){window.growMe||((window.growMe=function(e){window.growMe.push(e)}),(window.growMe=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTo2MDZlMjhjNC1mZTgxLTQ4ZDMtODdjOS1kOThhZDJkNWY4Njg=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();`
+          }}
+        />
       </body>
       </CSPostHogProvider>
     </html>
